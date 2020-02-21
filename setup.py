354afc139be609f5dd8c8d4cff39ce1c94956453
Noriyuki Abe <noriyukipy@gmail.com>
2020-02-21 22:23:29 +0900

    Fix setup.py to install package itself

--- setup.py
+++ setup.py
@@ -1,15 +1,16 @@
-from distutils.core import setup
+import setuptools
 
 
-setup(
+setuptools.setup(
     name="gptchat",
-    packages=["gptchat", "gptchat.gpt", "gptchat.chat", "gptchat.lib"],
+    packages=setuptools.find_packages(),
     install_requires=[
         "transformers==2.3.0",
         "fire==0.2.1",
         "tqdm==4.38.0",
         "mecab-python3==0.996.2",
         "responder==2.0.5",
+        "torch>=1.3.0",
     ],
     version="0.1.0",
     author="Noriyuki Abe",
