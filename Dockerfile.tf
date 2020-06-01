a5289fc77e56573513d8e14b5631ba4560cd4255
Noriyuki Abe <noriyukipy@gmail.com>
2020-06-02 01:58:08 +0900

    Add dependent library responder

--- Dockerfile.tf
+++ Dockerfile.tf
@@ -3,7 +3,7 @@ FROM tensorflow/tensorflow:2.1.0-gpu-py3
 RUN apt-get update && \
     apt-get install -y wget unzip language-pack-ja mecab libmecab-dev mecab-ipadic-utf8 jq
 
-RUN pip install jupyter==1.0.0 papermill==2.1.1 transformers==2.9.0 attrdict==2.0.1 fire==0.3.1 pyyaml==5.3.1
+RUN pip install jupyter==1.0.0 papermill==2.1.1 transformers==2.9.0 attrdict==2.0.1 fire==0.3.1 pyyaml==5.3.1 responder==2.0.5
 
 # Create cache directory used by Black
 RUN mkdir -p /root/.cache/black/19.10b0
